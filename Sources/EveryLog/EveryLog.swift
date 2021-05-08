3de14b98bbc76fa4bdd6e57bb2985b54f59a1edc
underthestars-zhy <zhuhaoyu0909@icloud.com>
2021-05-08 18:31:05 +0800

    New

--- Sources/EveryLog/EveryLog.swift
+++ Sources/EveryLog/EveryLog.swift
@@ -1,3 +1,40 @@
-struct EveryLog {
-    var text = "Hello, World!"
+import Foundation
+
+class EveryLog {
+    static let stande = EveryLog()
+    
+    let url:URL
+    let fileManager = FileManager()
+    
+    init() {
+        url = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    func addLog(_ log: String) throws {
+        let logUrl = url.appendingPathComponent("every.log")
+        let time = Date()
+        let timeString = date2String(time)
+        do {
+            let file = try String(contentsOfFile: logUrl.path)
+            let newFile = file + "\n" + "\(timeString) | " + log
+            try newFile.write(toFile: logUrl.path, atomically: true, encoding: .utf8)
+        } catch {
+            throw error
+        }
+    }
+    
+    func testLog(date: Date ,_ log: String) throws -> String {
+        let time = date
+        let timeString = date2String(time)
+        let newFile = "\(timeString) | " + log
+        return newFile
+    }
+    
+    func date2String(_ date:Date, dateFormat:String = "yyyy-MM-dd HH:mm:ss") -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale.init(identifier: "zh_CN")
+        formatter.dateFormat = dateFormat
+        let date = formatter.string(from: date)
+        return date
+    }
 }
--- Tests/EveryLogTests/EveryLogTests.swift
+++ Tests/EveryLogTests/EveryLogTests.swift
@@ -2,10 +2,11 @@
     @testable import EveryLog
 
     final class EveryLogTests: XCTestCase {
-        func testExample() {
+        func testLog1() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(EveryLog().text, "Hello, World!")
+            let date = Date()
+            XCTAssertEqual(try! EveryLog.stande.testLog(date: date, "test log"), "\(date) | test log")
         }
     }
